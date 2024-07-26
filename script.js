94c53de92af797577791b4b0a939c333e9c2c50e
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-07-26 01:28:01 +0000

    final

--- script.js
+++ script.js
@@ -15,52 +15,27 @@ checkButton.addEventListener("click", () => {
   let inputValue = input.value;
 
   if (inputValue == randomNum) {
-    [guess.textContent, 
-     input.disabled] = ["Congratulations!
-      You guessed the correct number.", true];
-    [checkButton.textContent, 
-      guess.style.color] = 
-      [ "Replay", "#333"];
-  } 
-  else if (input > randomNum && inputValue < 100) {
+    [guess.textContent, input.disabled] = ["Congratulations! You guessed the correct number.", true];
+    [checkButton.textContent, guess.style.color] = [ "Replay", "#333"];
+  } else if (inputValue > randomNum && inputValue < 100) {
 
-    [guess.textContent, 
-     remainChances.textContent] = 
-      ["Your guess is too HIGH HAHAHA", 
-      chance];
+    [guess.textContent, remainChances.textContent] = ["Your guess is too HIGH HAHAHA", chance];
     guess.style.color = "#333";
   } 
   
-  else if (input > randomNum && inputValue > 0) {
-             [guess.textContent, 
-              remainChances.textContent] = 
-               ["Your guess is
-               too LOWWW HAHAHA", chance];
+  else if (inputValue < randomNum && inputValue > 0) {
+             [guess.textContent, remainChances.textContent] = ["Your guess is too LOWWW HAHAHA", chance];
              guess.style.color = "#333";
-  }
-    
-  else 
-  {
-      [guess.textContent, 
-       remainChances.textContent] = 
-        ["Your number is 
-        invalid", chance];
+  }else {
+      [guess.textContent, remainChances.textContent] = ["Your number is invalid", chance];
       guess.style.color = "DEO6111";
     }
-
-    
   if (chance == 0) {
 
-      [checkButton.textContent, 
-       input.disabled, inputValue] = ["Replay"
-        , true, ""];
-      [guess.textContent, 
-       guess.style.color] = ["Game Over!!!!!", "#333"];
+      [checkButton.textContent, input.disabled, inputValue] = ["Replay", true, ""];
+      [guess.textContent, guess.style.color] = ["Game Over!!!!!", "#333"];
     }
-    
-    
-  if (chance < 0) {
+    if (chance < 0) {
       window.location.reload();
     }
-  
 });
